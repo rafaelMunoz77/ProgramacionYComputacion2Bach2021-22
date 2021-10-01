a29291791578094ae0f8bd9338a7ef3c89b4c229
Rafael Munoz <rafael.munoz77@gmail.com>
2021-10-01 10:45:32 +0200

    Capítulo 1

---
+++ src/tutorialJava/capitulo1/Ejemplo01_HolaMundo.java
@@ -0,0 +1,16 @@
+package tutorialJava.capitulo1;
+
+public class Ejemplo01_HolaMundo {
+
+	public static void main(String[] args) {
+		int numeroEntero1 = 3; // Declaración de una variable de tipo entero
+		int numeroEntero2 = 4;
+		int suma;
+		suma = numeroEntero1 + numeroEntero2;
+		System.out.println(numeroEntero1);
+		
+		long numeroLong = 5000000000l; 
+		
+	}     
+
+}
